de9fc81bcc253c9f53217e1a81f542407a93ff8b
Fejiroofficial <houseofjiro@gmail.com>
2018-09-11 11:09:18 +0100

    set up

---
+++ server/app.js
@@ -0,0 +1,27 @@
+import express from 'express';
+
+const app = express();
+
+const port = process.env.PORT || 7000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    success: 'true',
+    message: 'Welcome to home page',
+  });
+});
+
+
+app.use('*', (err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  res.status(statusCode).json({
+    status: 'fail',
+    message: err.message,
+  });
+});
+
+app.listen(port, () => console.log(`Running on port ${port}...`));
+export default app;
