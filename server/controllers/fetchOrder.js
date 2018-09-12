fd42a48810450005c6b8dd1b55d417ab0d37c814
Fejiroofficial <houseofjiro@gmail.com>
2018-09-12 15:03:36 +0100

    ft- API endpoint to get an order This is a commit for: - fetching a specific order endpoint. - test driven development for endpoint using mocha and chai.
    [Delivers #160449596]

---
+++ server/controllers/fetchOrder.js
@@ -0,0 +1,33 @@
+import allOrders from '../datastore/orders';
+
+const fetchOrders = (req, res, err) => {
+  const orderId = parseInt(req.params.id, 10);
+
+  for (let i = 0; i < allOrders.length; i++) {
+    if (allOrders[i].id === orderId) {
+      return res.status(200).json({
+        success: 'true',
+        message: 'order found',
+        order: allOrders[i],
+      });
+    } if (isNaN(orderId)) {
+      return res.status(400).json({
+        success: 'false',
+        message: 'hooops! params should be a number e.g /1 and not /a',
+      });
+    }
+    return res.status(404).json({
+      success: 'false',
+      message: 'This order does not exist in the database',
+    });
+  }
+
+  if (err) {
+    return res.status(500).json({
+      success: 'false',
+      message: err.message,
+    });
+  }
+};
+
+export default fetchOrders;
--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,6 +1,10 @@
 import postOrder from './postOrder';
 import updateStatus from './updateStatus';
 import getAllOrders from './getAllOrders';
+import fetchOrder from './fetchOrder';
 
-const orderCtrl = { postOrder, updateStatus, getAllOrders };
+const orderCtrl = {
+  postOrder, updateStatus, getAllOrders, fetchOrder,
+};
+
 export default orderCtrl;
