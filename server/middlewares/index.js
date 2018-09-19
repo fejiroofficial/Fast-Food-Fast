e86cbe4d43f2e8a3fa2f6950214255d1fa2cbcc6
Fejiroofficial <houseofjiro@gmail.com>
2018-09-19 18:58:50 +0100

    ch- challenge three set up This is a commit for installing dependencies required for application

--- server/middlewares/index.js
+++
@@ -1,9 +0,0 @@
-import validatePostOrder from './validatePostOrder';
-import validateUpdateStatus from './validateUpdateStatus';
-
-const middlewares = {
-  validatePostOrder,
-  validateUpdateStatus,
-};
-
-export default middlewares;
--- server/test/index.spec.js
+++
@@ -1,4 +0,0 @@
-import './app.spec';
-import './postOrder.Spec';
-import './updateStatus.spec';
-import './getAllOrders.spec';
