bc271f7d4395ca1e124563e36c4bbebb9ac69154
Fejiroofficial <houseofjiro@gmail.com>
2018-09-22 22:36:50 +0100

    API endpoint for user Login This is a commit for: - developing api endpoint - writing validation for endpoint - writing tests for endpoint
    [Delivers #160706464]

--- server/middlewares/index.js
+++ server/middlewares/index.js
@@ -1,8 +1,10 @@
 /* eslint linebreak-style: 0 */
 import validateSignup from './validateSignup';
+import validateLogin from './validateLogin';
 
 const middlewares = {
   validateSignup,
+  validateLogin,
 };
 
 export default middlewares;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,11 @@
 /* eslint linebreak-style: 0 */
 import express from 'express';
-import UserController from '../controllers/signup';
+import UserController from '../controllers/users';
 import middlewares from '../middlewares';
 
 const router = express.Router();
 
 router.post('/auth/signup', middlewares.validateSignup, UserController.signup);
+router.post('/auth/login', middlewares.validateLogin, UserController.login);
 
 export default router;
